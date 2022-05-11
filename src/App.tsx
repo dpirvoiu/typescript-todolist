fe3ca144f7cee8d847e54fca3ef2cb54a459c35a
dpirvoiu <adisor.pdw@gmail.com>
2022-05-11 11:41:40 +0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import InputField from './components/InputField';
-import {Todo} from './model';
+import { Todo } from './model';
 import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
-    const [todo, setTodo] = React.useState<string>("");
-    const [todos, setTodos] = React.useState<Todo[]>([]);
+  const [todo, setTodo] = React.useState<string>('');
+  const [todos, setTodos] = React.useState<Todo[]>([]);
 
-    const handleAdd = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (todo) {
-            setTodos([...todos, {id: Date.now(), todo, isDone: false}]);
-            setTodo("");
-        }
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo('');
     }
-    console.log(todos);
+  };
+  console.log(todos);
 
-    return (
-        <div className="App">
-            <span className="heading">Taskify</span>
-            <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-            <TodoList todos={todos} setTodos={setTodos}/>
-            {todos.map((t)=>(<li>{t.todo}</li>))}
-        </div>)
-}
+  return (
+    <div className='App'>
+      <span className='heading'>Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+      {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))}
+    </div>
+  );
+};
 
 export default App;
